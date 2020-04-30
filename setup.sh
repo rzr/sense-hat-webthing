43a60804b2c82d84489cc0408b7c316efbf40129
Philippe Coval <rzr@users.sf.net>
2020-04-30 14:14:24 +0200

    build: Add setup.sh helper
    Change-Id: I85e50f58d8495f55adaf371d837aab25988dceae
    Signed-off-by: Philippe Coval <rzr@users.sf.net>

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+#                                                              -*- Mode: Sh -*-
+# SPDX-License-Indentifier: MPL-2.0
+
+id=$(/usr/bin/lsb_release -i -s || echo "unknown")
+
+case "$id" in
+    "Debian" | "Raspbian" | "Ubuntu")
+        sudo=$(which sudo || echo"")
+        $sudo sync
+        $sudo apt-get update -y ||:
+        $sudo apt-get install -y python3-sense-hat
+        $sudo sync
+        ;;
+    *)
+        echo "warning: Not supported: $id trying pip"
+        sudo pip3 install sense_hat
+        ;;
+esac
